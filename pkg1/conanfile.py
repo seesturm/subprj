969e9eebf35adb376d98f713badc9d3eb98ad0de
Eduard Hasenleithner <eduard@hasenleithner.at>
2023-03-17 21:12:06 +0100

    Use git checkout

--- pkg1/conanfile.py
+++ pkg1/conanfile.py
@@ -1,53 +1,55 @@
 from conan import ConanFile
+from conan.tools.scm import Git
+from conan.tools.files import update_conandata
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
 
 class pkg1Recipe(ConanFile):
-    name = "pkg1"
-    version = "1.0"
+	name = "pkg1"
+	version = "1.0"
 
-    # Optional metadata
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of pkg1 package here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
+	# Optional metadata
+	license = "<Put the package license here>"
+	author = "<Put your name here> <And your email here>"
+	url = "<Package recipe repository url here, for issues about the package>"
+	description = "<Description of pkg1 package here>"
+	topics = ("<Put some tag here>", "<here>", "<and here>")
 
-    # Binary configuration
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+	# Binary configuration
+	settings = "os", "compiler", "build_type", "arch"
+	options = {"shared": [True, False], "fPIC": [True, False]}
+	default_options = {"shared": False, "fPIC": True}
 
-    # Sources are located in the same place as this recipe, copy them to the recipe
-    exports_sources = "CMakeLists.txt", "src/*", "include/*"
+	def layout(self):
+		self.folders.root = ".."
+		self.folders.subproject = "pkg1"
+		cmake_layout(self)
+	
+	def export(self):
+		git = Git(self, self.recipe_folder)
+		scm_url, scm_commit = git.get_url_and_commit()
+		update_conandata(self, {"sources": {"commit": scm_commit, "url": scm_url}})
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            self.options.rm_safe("fPIC")
+	def source(self):
+		git = Git(self)
+		sources = self.conan_data["sources"]
+		git.clone(url=sources["url"], target=".")
+		git.checkout(commit=sources["commit"])
 
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
+	def generate(self):
+		deps = CMakeDeps(self)
+		deps.generate()
+		tc = CMakeToolchain(self)
+		tc.generate()
 
-    def layout(self):
-        cmake_layout(self)
+	def build(self):
+		cmake = CMake(self)
+		cmake.configure()
+		cmake.build()
 
-    def generate(self):
-        deps = CMakeDeps(self)
-        deps.generate()
-        tc = CMakeToolchain(self)
-        tc.generate()
+	def package(self):
+		cmake = CMake(self)
+		cmake.install()
 
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
-
-    def package(self):
-        cmake = CMake(self)
-        cmake.install()
-
-    def package_info(self):
-        self.cpp_info.libs = ["pkg1"]
-
-    
+	def package_info(self):
+		self.cpp_info.libs = ["pkg1"]
